5ff572ad5588ab9c76b41564c57da29afe1abd17
MW-Limah <cdrmw16@gmail.com>
2025-06-20 21:59:00 +0100

    Remoção do SVGs padrão do Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Image src={'/'} alt=""/>
+      <Image src={'/logolivro.png'} width={100} height={100} alt=""/>
     </main>
   );
 }
